78fbac74b4d7b5334c75c154d01186cb13a8d546
anmarkosyan <anush.markos@gmail.com>
2020-06-15 14:10:55 +0400

    added tree traversal BFS method

---
+++ treeTraversal.js
@@ -0,0 +1,87 @@
+class Node{
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+class BinarySearchTrees {
+    constructor() {
+        this.root = null;
+
+    }
+    insert(val){
+        let newNode = new Node(val);
+        if(this.root === null) {
+            this.root = newNode;
+            return this;
+        }else{
+            let current = this.root;
+            while (true){
+                if(val === current.val) return undefined;
+                if(val < current.val){
+                    if(current.left === null){
+                        current.left = newNode;
+                        return this;
+                    }else{
+                        current = current.left;
+                    }
+                }else if(val > current.val){
+                    if(current.right === null){
+                        current.right = newNode;
+                        return this;
+                    }else{
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
+    find(val){
+        if(this.root === null) return false;
+        let current = this.root,
+            found = false;
+        while(current && !found){
+            if(val < current.val){
+                current = current.left;
+            } else if(val > current.val){
+                current = current.right;
+            }else{
+                found = true;     // return true;
+            }
+        }
+        if(!found) return false;  //return false
+        return current;
+    }
+    //we should go over the tree,visit every single node//output[10,5,13,2,7,11,18]
+    BFS(){
+        //make a variable that returned at the end
+        let node = this.root,
+            data = [],
+            queue = [];
+        //place the root node in the queue
+        queue.push(node);
+        //loop over the queue, if there is something
+        while(queue.length){
+            node = queue.shift();
+            data.push(node.val);
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+        return data;
+    }
+}
+const tree = new BinarySearchTrees();
+tree.insert(10);
+tree.insert(5);
+tree.insert(2);
+tree.insert(13);
+tree.insert(7);
+tree.insert(11);
+tree.insert(18);
+
+
+
+//      10
+//   5      13
+//2    7  11   18
